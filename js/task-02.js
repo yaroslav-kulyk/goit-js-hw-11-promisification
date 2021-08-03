2db82e48f2153f23748da6e8d8917331d41f47aa
Yaroslav Kulyk <kulyk.yaroslav@gmail.com>
2021-08-03 17:56:55 +0300

    Сделал домашнее задание

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
+  });
+};
+
+const logger = user => console.table(user);
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
